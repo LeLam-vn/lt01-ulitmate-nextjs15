a9701e1ec1063653e718b38a930cc872f646cbd2
LeLam <61404203+LeLam-vn@users.noreply.github.com>
2024-12-11 23:27:26 +0700

    019-Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Home = () => {
-  return (
-    <h1 className="text-3xl font-black text-white">
-      Welcome to the world of Next.js
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome to the World of Next JS</h1>
+    <h1 className="h1-bold font-space-grotesk">
+      Welcome to the World of Next JS
     </h1>
-  );
-};
+  </>
+);
 
 export default Home;
